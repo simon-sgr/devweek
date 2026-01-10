df9642a81d753497163c084b78879354f9ba789d
Simon <soesia18@htl-kaindorf.at>
2026-01-10 22:44:00 +0100

    shell fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
